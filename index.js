b32f2eaeae1f5e479e5c568510dd01952e5df5d6
juanfra270588 <juan.lopez@rimac.com.pe>
2025-05-02 10:59:34 -0500

    Cambio para probar CI- 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hola CI...probemos</h1>');
+  res.end('<h1>Hola CI...probemos por segunda vez</h1>');
 }).listen(port, () => {
   console.log(`Servidor escuchando en puerto ${port}`);
 });
